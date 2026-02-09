579632f405b3de634ba75d78b0b3b7a74bbe7843
herbgrace <thelen.alexander05@gmail.com>
2026-02-09 11:47:43 -0700

    Got basic connection to Mongo working

--- backend/DAL.js
+++ backend/DAL.js
@@ -1,1 +1,28 @@
-// we connecta da db here (with mongoose)
+const mongoose = require('mongoose');
+const { Schema, Model } = mongoose;
+mongoose.connect(process.env.MONGO_URI);
+
+const UserSchema = new Schema({
+    Id: Number,
+    Username: String,
+    Password: String,
+    ToDo: [{Id: Number, Task: String, Completed: Boolean}]
+})
+const User = mongoose.model('User', UserSchema);
+
+module.exports.DAL = {
+    getUserById : async function(id) {
+        return await User.find({"Id": id})
+    }
+}
+
+// User:
+//   Id: (Int)
+//   Username (string)
+//   Password (string - ideally hashed)
+//   To-Do (list of objects)
+
+// To-Dos
+//   Id (Int)
+//   Task (string)
+//   Completed (boolean)
--- backend/index.js
+++ backend/index.js
@@ -1,2 +1,6 @@
-console.log("Hello!")
-// We makea da backend here
+require('dotenv').config({path: "./credentials.env"});
+const { DAL } = require('./DAL');
+
+DAL.getUserById(1).then(data => {
+    console.log(data);
+})
