084c46e914fad8df14ef167caf9456fd792962a2
Makaden Espinosa <mespinosa@student.neumont.edu>
2026-02-16 23:27:19 -0700

    Basic user creation endpoint, express framework implementation, and signup.ejs

--- backend/index.js
+++ backend/index.js
@@ -1,2 +1,32 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+app.use(express.json());
+app.use(cors());
+
 require('dotenv').config({path: "./credentials.env"});
 const { DAL } = require('./DAL');
+
+
+
+
+app.post("/api/users", async (req, res) => {
+    try {
+        const newUser = {
+            Username: req.body.Username,
+            Password: req.body.Password
+        }
+
+        const createdUser = await DAL.createUser(newUser);
+        res.json(createdUser);  
+    } catch (err) {
+        res.status(400).json({error: "Invalid request body"});
+        console.error(err);
+        return;
+    }
+});
+
+        
+app.listen(3001, () => {
+    console.log("Backend is running on port 3001");
+});
--- frontend/index.js
+++ frontend/index.js
@@ -1,5 +1,54 @@
 // we makea da frontend here
 
-document.getElementById("btn").addEventListener("click", () => {
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true})); 
+app.set("view engine", "ejs");
+
+
+/*document.getElementById("btn").addEventListener("click", () => {
     alert("You clicked me!");
+});*/
+
+
+// POST to create a new user. 
+app.post("/users", async (req, res) => {
+    try {
+        const newUser = {
+            Username: req.body.Username,
+            Password: req.body.Password
+        }
+
+        const response = await fetch("http://localhost:3001/api/users", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(newUser)
+        });
+
+        const data = await response.json();
+        res.json(data);
+        // Return the created user as a response
+        // Here I could use the createdUser's id to be stored locally as a login token or something,
+        //  but for now I'll just return the created user as a response. I will talk to yall tmr (9/17/2026) about 
+        //  how we want to handle logins and sessions and stuff.
+    } catch (err) {
+        res.status(400).json({error: "Invalid request body"});
+        console.error(err);
+        return;
+    }
+    
+});
+
+app.get("/users/signup", async (req, res) => {
+    res.render("signup.ejs");
+});
+    
+
+
+app.listen(3000, () => {
+    console.log("Frontend is running on port 3000");
 });
